8dba507bfd60dad96020d36ff73c910d8febb35b
Brendan Collins <68344516+brendanc490@users.noreply.github.com>
2023-01-19 10:52:25 -0500

    Add files via upload

--- Custom/load.js
+++ Custom/load.js
@@ -1,14 +1,14 @@
-import red from './prebuilt/red.JSON' assert { type: "json" };
+import red from './patterns/red.JSON' assert { type: "json" };
 scenes['red'] = red['scenes']['default']
 
-import green from './prebuilt/green.JSON' assert { type: "json" };
+import green from './patterns/green.JSON' assert { type: "json" };
 scenes['green'] = green['scenes']['default']
 
-import blue from './prebuilt/blue.JSON' assert { type: "json" };
+import blue from './patterns/blue.JSON' assert { type: "json" };
 scenes['blue'] = blue['scenes']['default']
 
-import white from './prebuilt/white.JSON' assert { type: "json" };
+import white from './patterns/white.JSON' assert { type: "json" };
 scenes['white'] = white['scenes']['default']
 
-import grille from './prebuilt/grille.JSON' assert { type: "json" };
+import grille from './patterns/grille.JSON' assert { type: "json" };
 scenes['grille'] = grille['scenes']['default']
