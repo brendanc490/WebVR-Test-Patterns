322f276c2555786bd98483925c5b97137e131a65
Brendan Collins <68344516+brendanc490@users.noreply.github.com>
2023-04-27 23:01:05 -0400

    Update load.js

--- Custom/load.js
+++ Custom/load.js
@@ -14,3 +14,18 @@ scenes['white'] = white['scenes']['default']
 
 import grille from './patterns/grille.JSON' assert { type: "json" };
 scenes['grille'] = grille['scenes']['default']
+
+import line from './patterns/line.JSON' assert { type: "json" };
+scenes['line'] = line['scenes']['line']
+
+import checkerboard_w from './patterns/checkerboard (w).JSON' assert { type: "json" };
+scenes['checkerboard (w)'] = checkerboard_w['scenes']['checkerboard (w)']
+
+import checkerboard_b from './patterns/checkerboard (b).JSON' assert { type: "json" };
+scenes['checkerboard (b)'] = checkerboard_b['scenes']['checkerboard (b)']
+
+import dot_array from './patterns/dot array.JSON' assert { type: "json" };
+scenes['dot array'] = checkerboard_w['scenes']['dot array']
+
+import crosshair from './patterns/crosshair.JSON' assert { type: "json" };
+scenes['crosshair'] = checkerboard_w['scenes']['crosshair']
