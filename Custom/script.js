71df09f112e8b051f45671805861d723508270e9
VRAR DIDSR <brendanc490@gmail.com>
2023-06-20 14:46:10 -0400

    Debug Push

--- Custom/script.js
+++ Custom/script.js
@@ -281,3 +281,30 @@ function cancelDefault (e) {
   e.stopPropagation()
   return false
 }
+
+// SOURCE    https://stackoverflow.com/questions/16616722/sending-all-javascript-console-output-into-a-dom-element 
+
+var baseLogFunction = console.log;
+console.log = function(){
+    baseLogFunction.apply(console, arguments);
+
+    var args = Array.prototype.slice.call(arguments);
+    for(var i=0;i<args.length;i++){
+        var node = createLogNode(args[i]);
+        document.querySelector("#debug").appendChild(node);
+    }
+
+}
+
+function createLogNode(message){
+    var node = document.createElement("div");
+    node.style.overflowY = 'auto';
+    var textNode = document.createTextNode(message);
+    node.appendChild(textNode);
+    return node;
+}
+
+window.onerror = function(message, url, linenumber) {
+    console.log("JavaScript error: " + message + " on line " +
+        linenumber + " for " + url);
+}
