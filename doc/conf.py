3850e6a45d5075206b30bc1da86d4db8855e1030
Brendan Collins <68344516+brendanc490@users.noreply.github.com>
2023-08-01 14:49:10 -0400

    Update conf.py

--- doc/conf.py
+++ doc/conf.py
@@ -31,11 +31,19 @@ author = 'Brendan Collins, Miguel Lago, Ryan Beams'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['sphinx.ext.intersphinx','sphinx.ext.autodoc',"sphinx_rtd_theme"
-]
+extensions = ['sphinx.ext.autodoc','sphinx_rtd_theme']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
+
+# The suffix(es) of source filenames.
+# You can specify multiple suffix as a list of string:
+#
+# source_suffix = ['.rst', '.md']
+source_suffix = '.rst'
+
+# The master toctree document.
+master_doc = 'index'
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
