7a86ddd36a37ba13da793b2c40ac59daa1c081c0
Brendan Collins <68344516+brendanc490@users.noreply.github.com>
2023-08-08 14:32:15 -0400

    Update conf.py

--- doc/conf.py
+++ doc/conf.py
@@ -27,7 +27,7 @@ author = 'Brendan Collins, Miguel Lago, Ryan Beams'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['sphinx.ext.intersphinx',"sphinx_rtd_theme","sphinxcontrib.video"
+extensions = ['sphinx.ext.intersphinx',"sphinx_pdj_theme","sphinxcontrib.video"
 ]
 
 # Add any paths that contain templates here, relative to this directory.
@@ -44,7 +44,9 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = "pyramid"
+html_theme = "sphinx_pdj_theme"
+
+html_theme_path = [sphinx_pdj_theme.get_html_theme_path()]
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
