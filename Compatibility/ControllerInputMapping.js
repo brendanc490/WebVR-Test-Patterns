11f56c79990dfac86fa96f80d1c3ecab34b93677
VRAR DIDSR <brendanc490@gmail.com>
2024-03-07 18:15:50 -0500

    Debugging

--- Compatibility/ControllerInputMapping.js
+++ Compatibility/ControllerInputMapping.js
@@ -9,7 +9,7 @@ const generic = data[0]['generic']
 
 conLeft.addEventListener('buttondown', function (evt) {
     console.log("left")
-    console.log(JSON.stringify(evt.type))
+    console.log(JSON.stringify(evt.detail.id))
     if(scheme == 'windows-mixed-reality'){
         conLeft.dispatchEvent(new CustomEvent(windows['left'][evt.detail.id]+'down', {detail: true}))
     }  else if(scheme == 'oculus-touch' || scheme == 'oculus-touch-v2' || scheme == 'oculus-touch-v3' || scheme == 'meta-quest-touch-pro') {
@@ -27,7 +27,7 @@ conLeft.addEventListener('buttondown', function (evt) {
 
 conRight.addEventListener('buttondown', function (evt) {
     console.log("right")
-    console.log(JSON.stringify(evt.type))
+    console.log(JSON.stringify(evt.detail.id))
     if(scheme == 'windows-mixed-reality'){
         conRight.dispatchEvent(new CustomEvent(windows['right'][evt.detail.id]+'down', {detail: true}))
     }  else if(scheme == 'oculus-touch' || scheme == 'oculus-touch-v2' || scheme == 'oculus-touch-v3' || scheme == 'meta-quest-touch-pro') {
