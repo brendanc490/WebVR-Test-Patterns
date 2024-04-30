c0ca519f84cc7ca1d9981655ec91f5afa8f93808
GitHub Action <action@github.com>
2024-04-30 18:18:28 +0000

    Update documentation

--- Custom/ControllerInputMapping.js
+++ Custom/ControllerInputMapping.js
@@ -1,65 +1,16 @@
-/* 
-    Reads in controller input profiles and sets event listeners.
-    Event listeners will fire and dispatch correctly encoded event to controller button listeners
-    Controller schemes are windows, oculus touch, oculus go, vive, vive focus, and a catch all that contains all buttons
-    
-*/
-
-//import data from '../Compatibility/controller_profiles.json' assert { type: "json" };
+//import data from './controller_profiles.json' assert { type: "json" };
 const windows = data[0]['windows']
 const oc_touch = data[0]['oc_touch']
 const oc_go = data[0]['oc_go']
 const vive = data[0]['vive']
 const vive_focus = data[0]['vive_focus']
+const magic = data[0]['magic']
 // contains all possible buttons and axes
 const generic = data[0]['generic']
 
-let isRight, isLeft, scheme;
-function findControls(){
-    isRight = !(conRight.getAttribute("position").x == 0 && conRight.getAttribute("position").y == 0 && conRight.getAttribute("position").z == 0);
-    isLeft = !(conLeft.getAttribute("position").x == 0 && conLeft.getAttribute("position").y == 0 && conLeft.getAttribute("position").z == 0);
-    if(isRight){
-        if(conRight.components['tracked-controls'].attrValue.hasOwnProperty("id") && conRight.components['tracked-controls'].attrValue.id != ""){
-            scheme = conRight.components['tracked-controls-webxr'].attrValue.id
-            clearInterval(controlsInterval)
-        } else if(conRight.components['tracked-controls'].attrValue.hasOwnProperty("idPrefix") && conRight.components['tracked-controls'].attrValue.idPrefix != ""){
-            scheme = conRight.getAttribute("tracked-controls").idPrefix;
-            clearInterval(controlsInterval)
-        } else {
-            //executeQueries(queryPrefix)
-            scheme = null
-        }
-        
-    } else if(isLeft) {
-        if(conLeft.components['tracked-controls'].attrValue.hasOwnProperty("id") && conLeft.components['tracked-controls'].attrValue.id != ""){
-            scheme = conLeft.components['tracked-controls-webxr'].attrValue.id
-            clearInterval(controlsInterval)
-        } else if(conLeft.components['tracked-controls'].attrValue.hasOwnProperty("idPrefix") && conLeft.components['tracked-controls'].attrValue.idPrefix != ""){
-            scheme = conLeft.getAttribute("tracked-controls").idPrefix;
-            clearInterval(controlsInterval)
-            
-        } else {
-            scheme = null
-        }
-    }
-    
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 conLeft.addEventListener('buttondown', function (evt) {
-    console.log(evt.detail.id)
+    console.log("left")
+    console.log(JSON.stringify(evt.detail.id))
     if(scheme == 'windows-mixed-reality'){
         conLeft.dispatchEvent(new CustomEvent(windows['left'][evt.detail.id]+'down', {detail: true}))
     }  else if(scheme == 'oculus-touch' || scheme == 'oculus-touch-v2' || scheme == 'oculus-touch-v3' || scheme == 'meta-quest-touch-pro') {
@@ -70,13 +21,18 @@ conLeft.addEventListener('buttondown', function (evt) {
         conLeft.dispatchEvent(new CustomEvent(vive['left'][evt.detail.id]+'down', {detail: true}))
     } else if(scheme == 'htc-vive-focus') {
         conLeft.dispatchEvent(new CustomEvent(vive_focus['left'][evt.detail.id]+'down', {detail: true}))
+    } else if(scheme == 'magicleap-one') {
+        conLeft.dispatchEvent(new CustomEvent(magic['left'][evt.detail.id]+'down', {detail: true}))
     } else {
         conLeft.dispatchEvent(new CustomEvent(generic['left'][evt.detail.id]+'down', {detail: true}))
     }
 });
 
 conRight.addEventListener('buttondown', function (evt) {
-    console.log(evt.detail.id)
+    console.log("right")
+    console.log(JSON.stringify(evt.detail.id))
+    console.log(scheme)
+    console.log(queriesFound)
     if(scheme == 'windows-mixed-reality'){
         conRight.dispatchEvent(new CustomEvent(windows['right'][evt.detail.id]+'down', {detail: true}))
     }  else if(scheme == 'oculus-touch' || scheme == 'oculus-touch-v2' || scheme == 'oculus-touch-v3' || scheme == 'meta-quest-touch-pro') {
@@ -87,7 +43,12 @@ conRight.addEventListener('buttondown', function (evt) {
         conRight.dispatchEvent(new CustomEvent(vive['right'][evt.detail.id]+'down', {detail: true}))
     } else if(scheme == 'htc-vive-focus') {
         conRight.dispatchEvent(new CustomEvent(vive_focus['right'][evt.detail.id]+'down', {detail: true}))
+    } else if(scheme == 'magicleap-one') {
+        console.log(magic['right'][evt.detail.id]+'down')
+        conRight.dispatchEvent(new CustomEvent(magic['right'][evt.detail.id]+'down', {detail: true}))
     } else {
+        console.log(JSON.stringify(generic['right'][evt.detail.id]+'down'))
+        console.log(queriesFound)
         conRight.dispatchEvent(new CustomEvent(generic['right'][evt.detail.id]+'down', {detail: true}))
     }
 });
@@ -103,6 +64,8 @@ conLeft.addEventListener('buttonup', function (evt) {
         conLeft.dispatchEvent(new CustomEvent(vive['left'][evt.detail.id]+'up', {detail: true}))
     } else if(scheme == 'htc-vive-focus') {
         conLeft.dispatchEvent(new CustomEvent(vive_focus['left'][evt.detail.id]+'up', {detail: true}))
+    } else if(scheme == 'magicleap-one') {
+        conLeft.dispatchEvent(new CustomEvent(magic['left'][evt.detail.id]+'up', {detail: true}))
     } else {
         conLeft.dispatchEvent(new CustomEvent(generic['left'][evt.detail.id]+'up', {detail: true}))
     }
@@ -119,6 +82,8 @@ conRight.addEventListener('buttonup', function (evt) {
         conRight.dispatchEvent(new CustomEvent(vive['right'][evt.detail.id]+'up', {detail: true}))
     } else if(scheme == 'htc-vive-focus') {
         conRight.dispatchEvent(new CustomEvent(vive_focus['right'][evt.detail.id]+'up', {detail: true}))
+    } else if(scheme == 'magicleap-one') {
+        conRight.dispatchEvent(new CustomEvent(magic['right'][evt.detail.id]+'up', {detail: true}))
     } else {
         conRight.dispatchEvent(new CustomEvent(generic['right'][evt.detail.id]+'up', {detail: true}))
     }
@@ -157,6 +122,14 @@ conLeft.addEventListener('axismove', function (evt) {
         })
         conLeft.dispatchEvent(trackpadmove)
     }  else if(scheme == 'htc-vive' || scheme == 'htc-vive-focus-plus' || scheme == 'htc-vive-focus') {
+        let trackpadmove = new CustomEvent('trackpadmoved', {
+            detail: {
+                x: evt.detail.axis[0],
+                y: evt.detail.axis[1]
+            }
+        })
+        conLeft.dispatchEvent(trackpadmove)
+    } else if(scheme == "magicleap-one") {
         let trackpadmove = new CustomEvent('trackpadmoved', {
             detail: {
                 x: evt.detail.axis[0],
@@ -215,6 +188,14 @@ conRight.addEventListener('axismove', function (evt) {
         })
         conRight.dispatchEvent(trackpadmove)
     }  else if(scheme == 'htc-vive' || scheme == 'htc-vive-focus-plus' || scheme == 'htc-vive-focus') {
+        let trackpadmove = new CustomEvent('trackpadmoved', {
+            detail: {
+                x: evt.detail.axis[0],
+                y: evt.detail.axis[1]
+            }
+        })
+        conRight.dispatchEvent(trackpadmove)
+    } else if(scheme == "magicleap-one") {
         let trackpadmove = new CustomEvent('trackpadmoved', {
             detail: {
                 x: evt.detail.axis[0],
